7006b5faf49592e30ad9ad3b1e5e9d4f9d0c609c
magic <magic10r.program@gmail.com>
2024-11-16 21:30:24 +0900

    update reference

---
+++ go.mod
@@ -0,0 +1,15 @@
+module helloworld
+
+go 1.23.3
+
+require (
+	github.com/mt-magic/go_calculator v0.1.0
+	rsc.io/quote v1.5.2
+)
+
+require (
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
+
+replace github.com/mt-magic/go_calculator => ../go_calculator
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+// import "fmt"
+import (
+	"fmt"
+
+	calculator "github.com/mt-magic/go_calculator"
+	"rsc.io/quote"
+)
 
 func main() {
-    fmt.Println("Hello World!")
+    // fmt.Println("Hello World!")
+
+    // update
+    total := calculator.Sum(30, 55)
+    fmt.Println(total)
+    fmt.Println("Version: ", calculator.Version)
+
+    // update add
+    fmt.Println(quote.Hello())
+
+    // Error
+    // total := go_calculator.internalSum(5)
+    // fmt.Println(total)
+    // fmt.Println("Version: ", go_calculator.logMessage)
 }
